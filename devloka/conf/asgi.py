77a1f819f5d46650ec6862acb3dba7de789826bf
mohitprajapat2001 <108051766+mohitprajapat2001@users.noreply.github.com>
2025-07-27 18:01:21 +0530

    Update asgi.py

--- devloka/conf/asgi.py
+++ devloka/conf/asgi.py
@@ -1,17 +1,21 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
 import django
+
+django.setup()
+
+from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from codespace.routing import websocket_urlpatterns
-from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-django.setup()
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
